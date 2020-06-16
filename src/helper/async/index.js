7a1466ef0b1d88da4ce0a0adcbdfe86a5398c065
Rejsal <rejsal.tblr@gmail.com>
2020-06-16 19:34:55 +0530

    auth flow, async storage and sample test for api, props, state and library mock

--- src/helper/async/index.js
+++ src/helper/async/index.js
@@ -1,13 +1,16 @@
 import AsyncStorage from "@react-native-community/async-storage";
 
+//get login email for login
 export async function isSignedIn() {
     return await AsyncStorage.getItem('email');
 }
 
+//set login email for login
 export async function setEmail(value) {
     return await AsyncStorage.setItem('email',value);
 }
 
+//remove email for logout
 export async function removeEmail(value) {
     return await AsyncStorage.removeItem('email');
 }
